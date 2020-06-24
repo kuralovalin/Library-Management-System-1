ba0622477c5d8fc6234c61514c4b52f59be4a69d
Oğuzhan Şentürk <osenturk@gtu.edu.tr>
2020-06-25 01:22:52 +0300

    User Interface updated

--- Library_Management_System/src/UserInterface.java
+++ Library_Management_System/src/UserInterface.java
@@ -1,5 +1,14 @@
 public interface UserInterface {
+    /**
+     * This methods shows all book in the system
+     */
     void showBook();
+    /**
+     * This methods shows all table in the system
+     */
     void showTable();
+    /**
+     * This methods search given book name in the system
+     */
     void searchBook();
 }
