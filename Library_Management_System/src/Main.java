68ee2c76ed71cecdccde9935a24608ba5bd32b58
Oğuzhan Şentürk <osenturk@gtu.edu.tr>
2020-06-25 01:24:02 +0300

    Driver Main Class added

--- Library_Management_System/src/Main.java
+++ Library_Management_System/src/Main.java
@@ -1,7 +1,16 @@
+/**
+ * Drive class
+ */
 public class Main {
+    /**
+     * Drive method
+     * @param args args
+     */
     public static void main(String[] args) {
 
         LibrarySystem librarySystem = new LibrarySystem();
         librarySystem.mainMenu();
+
     }
+
 }
